a5691184fe421d722bcb7b65dde7a22b302c5970
timo <timofey.luin@gmail.com>
2026-03-10 17:21:08 +0000

    copy jolt-sdk

---
+++ jolt-sdk/src/platform/mod.rs
@@ -0,0 +1,18 @@
+pub mod alloc;
+pub use alloc::*;
+#[cfg(feature = "random")]
+pub mod random;
+#[cfg(feature = "random")]
+pub use random::*;
+
+pub mod print;
+pub use print::*;
+
+pub mod cycle_tracking;
+pub use cycle_tracking::*;
+
+#[cfg(all(
+    feature = "malloc-shim",
+    any(target_arch = "riscv32", target_arch = "riscv64")
+))]
+mod malloc_shim;
